39fc6181d4c0e3bafd58eefb04c24e5f9e7528e6
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-15 00:19:29 +0300

    Finished Task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,42 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const domEls = {
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  amountInput: document.querySelector('#controls>input'),
+  boxesDiv: document.querySelector('#boxes'),
+};
+
+function createBoxes(amount) {
+  let divCollectionMarkup = '';
+
+  for (let i = 0; i < amount; i++) {
+    const divWidthHeight = 30 + i * 10;
+    const randomHexColor = getRandomHexColor();
+
+    divCollectionMarkup += `<div style = 
+      'background-color: ${randomHexColor}; 
+      width: ${divWidthHeight}px;
+      height: ${divWidthHeight}px;'>
+    </div>`;
+  }
+
+  domEls.boxesDiv.insertAdjacentHTML('beforeend', divCollectionMarkup);
 }
+
+function destroyBoxes() {
+  domEls.boxesDiv.innerHTML = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onControlButtonsClick(event) {
+  event.currentTarget === domEls.createBtn
+    ? createBoxes(Number(domEls.amountInput.value))
+    : destroyBoxes();
+}
+
+domEls.createBtn.addEventListener('click', onControlButtonsClick);
+domEls.destroyBtn.addEventListener('click', onControlButtonsClick);
