72077f042f62b77de9d059d36ca1d8fdd2da7752
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-14 13:11:58 +0300

    Finished Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const parentNode = document.querySelector('#ingredients');
+const nodesToAppend = ingredients.map(value => {
+  const nodeToAppend = document.createElement('li');
+
+  nodeToAppend.textContent = value;
+  return nodeToAppend;
+});
+
+parentNode.append(...nodesToAppend);
