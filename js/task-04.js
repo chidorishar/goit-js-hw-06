eaa583a834a6f3e84d05524f56d6c639f29165ff
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-14 20:26:44 +0300

    Finished Task-4

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+const domEls = {
+  incrBtn: document.querySelector('[data-action="increment"]'),
+  decrBtn: document.querySelector('[data-action="decrement"]'),
+  spanWithValue: document.querySelector('#value'),
+};
+
+let counterValue = 0;
+const COUNTER_STEP = 1;
+
+const onCntrlBtnClick = function (event) {
+  counterValue += event.currentTarget === domEls.incrBtn ? COUNTER_STEP : -COUNTER_STEP;
+  domEls.spanWithValue.textContent = counterValue;
+};
+
+domEls.incrBtn.addEventListener('click', onCntrlBtnClick);
+domEls.decrBtn.addEventListener('click', onCntrlBtnClick);
