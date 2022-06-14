bf64805a266fe4c7ec339173572d02785d6dc9e9
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-14 23:31:48 +0300

    Finished Task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form');
 
+const onFormSubmit = function (event) {
+  const formData = new FormData(event.currentTarget);
+
+  event.preventDefault();
+
+  if ([...formData.values()].some(value => value === '')) {
+    alert('All fields must be filled!');
+    return;
+  }
+
+  formEl.reset();
+  console.log(Object.fromEntries(formData));
+};
+
+formEl.addEventListener('submit', onFormSubmit);
