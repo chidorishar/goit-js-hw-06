5e3a26eea6ba005c07bfac0ccfa915728629b7ab
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-14 23:45:00 +0300

    Finished Task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const domEls = {
+  cntrlBtn: document.querySelector('.change-color'),
+  feedbackSpan: document.querySelector('.color'),
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+function onControlButtonClick() {
+  const randomHexColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomHexColor;
+  domEls.feedbackSpan.textContent = randomHexColor;
+}
+
+domEls.cntrlBtn.addEventListener('click', onControlButtonClick);
